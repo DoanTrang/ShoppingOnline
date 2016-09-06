578e6d039cf3f4a4db79c4efa060e49faa26f126
TrangMoon <TrangMoon@TRANGMOON>
2016-09-06 14:36:30 +0700

    Add project files.

---
+++ ShoppingOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingOnline.Startup))]
+namespace ShoppingOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
